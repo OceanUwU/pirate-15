f66cb45f515955eb2a993881de7c0802a673685b
fferniv <ffvasquez30@gmail.com>
2024-07-22 13:42:43 -0700

    sprite sheet for crow walk and idle

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,28 +6,28 @@ extends CharacterBody2D
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 
-#func _ready():
-	#update_animation_parameter(starting_direction)
+func _ready():
+	update_animation_parameter(starting_direction)
 func _physics_process(_delta):
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up") 
 		)
 		
-	#update_animation_parameter(input_direction)
+	update_animation_parameter(input_direction)
 	
 	velocity = input_direction * move_speed
-	## pick_move_state()
+	pick_move_state()
 	move_and_slide()
 	
 
 func update_animation_parameter(move_input : Vector2):
 	if(move_input != Vector2.ZERO):
-		animation_tree.set("parameters/Walk/blend_position", move_input)
-		animation_tree.set("parameters/Idle/blend_position", move_input)
+		animation_tree.set("parameters/walk_forward/blend_position", move_input)
+		animation_tree.set("parameters/idle/blend_position", move_input)
 		
 func pick_move_state():
 	if(velocity != Vector2.ZERO):
-		state_machine.travel("Walk")
+		state_machine.travel("walk_forward")
 	else:
-		state_machine.travel("Idle")
+		state_machine.travel("idle")
