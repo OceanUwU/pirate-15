76d4d5db8f04fdf9ef8e26ecd6bc8070a1506017
MatthewMa929 <MatthewMa929@Gmail.com>
2024-07-22 22:24:06 -0400

    raycast moving

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -9,11 +9,13 @@ enum Direction { UP, UP_RIGHT, DOWN, DOWN_RIGHT }
 @export var roam_speed: float
 @export var raycast: RayCast2D 
 @export var line: Line2D #Just visual to keep track of raycast
-@export var starting_ang = 60
-@export var end_ang = -60
+@export var starting_ang: float = 60
+@export var end_ang: float = -60
 
-@onready var raycast_arr = [raycast]
-@onready var angle = starting_ang
+@onready var raycast_arr: Array[RayCast2D] = [raycast]
+@onready var raycast_ang: float = starting_ang
+
+@onready var line_arr: Array[Line2D] = [line]
 
 #Light is Area2D, darkness is revealed when light collides with it. 
 var following_path: bool = true
@@ -34,6 +36,7 @@ func _physics_process(delta: float) -> void:
 			print(i)
 
 func set_dir(angle: float) -> void:
+	change_raycast_rotation(angle) #TEMP SOLUTION (WHEN 8 DIRECTION ADDED WILL CHANGE)
 	var direction_before: String = direction
 	angle = fmod(angle + TAU * 6, TAU) - PI
 	direction = "down" if angle < 0.01 else "up"
@@ -46,23 +49,27 @@ func set_dir(angle: float) -> void:
 		animation_player.play(action + "_" + direction)
 
 func set_up_raycast():
-	raycast.rotation = deg_to_rad(starting_ang)
-	line.rotation = deg_to_rad(starting_ang)
-	while angle > end_ang:
-		angle -= 10
+	while raycast_ang > end_ang:
+		raycast_ang -= 10
 		duplicate_line()
 		duplicate_raycast()
+	change_raycast_rotation(deg_to_rad(starting_ang))
 	#duplicate_raycast()
 
 func duplicate_raycast():
 	var new_raycast = raycast.duplicate()
 	new_raycast.position = raycast.position
-	new_raycast.rotation = deg_to_rad(angle)
 	character.add_child(new_raycast)
 	raycast_arr.append(new_raycast)
 
 func duplicate_line():
 	var new_line = line.duplicate()
 	new_line.position = line.position
-	new_line.rotation = deg_to_rad(angle)
 	character.add_child(new_line)
+	line_arr.append(new_line)
+
+func change_raycast_rotation(angle: float):
+	for i in range(raycast_arr.size()):
+		raycast_arr[i].rotation = angle
+		line_arr[i].rotation = angle
+		angle -= deg_to_rad(10)
